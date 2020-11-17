b50b9229b7ccae590b35181ba349633308fba670
Liderk <vismoke@yandex.ru>
2020-11-17 17:43:15 +0700

    Cancel update

--- foodgram/urls.py
+++ foodgram/urls.py
@@ -23,13 +23,11 @@ from django.contrib.flatpages import views
 
 
 urlpatterns = [
-    path('about/', include('django.contrib.flatpages.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
     path('auth/', include("django.contrib.auth.urls")),
     path('api/', include('api.urls')),
-    path('about-author/', views.flatpage,
-         {'url': '/about-author/'}, name='about-author'),
+    path('about-us/', views.flatpage, {'url': '/about-us/'}, name='about'),
     path('about-spec/', views.flatpage, {'url': '/about-spec/'}, name='spec'),
     path('', include("recipes.urls"))
 ]
